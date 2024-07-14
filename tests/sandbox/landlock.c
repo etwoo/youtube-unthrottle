149727887d8e1d098d1b3d660e35afa05d9e1772
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2024-07-13 21:06:11 -0700

    split into separate landlock.c, seccomp.c tests
    This makes it easier to avoid issues where the landlock and seccomp
    tests interfere with one another, if run consecutively within a single
    process.
    
    Also, reorganize sanitizer flags a bit, since we don't want to link ASan
    into these unit tests and cause more LSan/seccomp conflicts.

---
+++ tests/sandbox/landlock.c
@@ -0,0 +1,133 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE /* for O_TMPFILE in open() */
+#endif
+#include <fcntl.h>
+#undef _GNU_SOURCE /* revert for any other includes */
+
+#include "greatest.h"
+#include "landlock.h"
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+TEST before_landlock_filesystem(void) {
+	int fd = open(__FILE__, O_RDONLY);
+	ASSERT_GTE(fd, 0);
+	int rc = close(fd);
+	ASSERT_EQ(rc, 0);
+
+	int tmpfd = open(P_tmpdir, O_TMPFILE | O_EXCL | O_RDWR, 0);
+	ASSERT_GTE(tmpfd, 0);
+	rc = close(tmpfd);
+	ASSERT_EQ(rc, 0);
+
+	PASS();
+}
+
+TEST before_landlock_network(void) {
+	int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	ASSERT_GTE(sfd, 0);
+
+	struct sockaddr_in sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(443);
+	inet_pton(AF_INET, "93.184.215.14", &sa.sin_addr); /* example.com */
+
+	int rc = connect(sfd, &sa, sizeof(sa));
+	ASSERT_GTE(rc, 0);
+
+	rc = close(sfd);
+	ASSERT_EQ(rc, 0);
+
+	PASS();
+}
+
+SUITE(before_landlock) {
+	RUN_TEST(before_landlock_filesystem);
+	RUN_TEST(before_landlock_network);
+}
+
+TEST setup_partial_landlock(void) {
+	const char *paths[] = {
+		P_tmpdir,
+	};
+	landlock_apply(paths, 1, 443);
+	PASS();
+}
+
+TEST partial_landlock_filesystem(void) {
+	int fd = open(__FILE__, O_RDONLY);
+	ASSERT_LT(fd, 0);
+
+	int tmpfd = open(P_tmpdir, O_TMPFILE | O_EXCL | O_RDWR, 0);
+	ASSERT_GTE(tmpfd, 0);
+	int rc = close(tmpfd);
+	ASSERT_EQ(rc, 0);
+
+	PASS();
+}
+
+SUITE(partial_landlock) {
+	RUN_TEST(setup_partial_landlock);
+	RUN_TEST(partial_landlock_filesystem);
+	RUN_TEST(before_landlock_network); /* reuse before_* network check */
+}
+
+TEST setup_full_landlock(void) {
+	landlock_apply(NULL, 0, 0);
+	PASS();
+}
+
+TEST after_landlock_filesystem(void) {
+	int fd = open(__FILE__, O_RDONLY);
+	ASSERT_LT(fd, 0);
+
+	int tmpfd = open(P_tmpdir, O_TMPFILE | O_EXCL | O_RDWR, 0);
+	ASSERT_LT(tmpfd, 0);
+
+	PASS();
+}
+
+TEST after_landlock_network(void) {
+	int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	ASSERT_GTE(sfd, 0);
+
+	struct sockaddr_in sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(443);
+	inet_pton(AF_INET, "93.184.215.14", &sa.sin_addr); /* example.com */
+
+	int rc = connect(sfd, &sa, sizeof(sa));
+	ASSERT_EQ(rc, -1);
+	ASSERT_EQ(errno, EACCES);
+
+	rc = close(sfd);
+	ASSERT_EQ(rc, 0);
+
+	PASS();
+}
+
+SUITE(full_landlock) {
+	RUN_TEST(setup_full_landlock);
+	RUN_TEST(after_landlock_filesystem);
+	RUN_TEST(after_landlock_network);
+}
+
+GREATEST_MAIN_DEFS();
+
+int
+main(int argc, char **argv)
+{
+	GREATEST_MAIN_BEGIN();
+
+	RUN_SUITE(before_landlock);
+	RUN_SUITE(partial_landlock);
+	RUN_SUITE(full_landlock);
+
+	GREATEST_MAIN_END();
+}
--- tests/sandbox/seccomp.c
+++ tests/sandbox/seccomp.c
@@ -4,134 +4,15 @@
 #include <fcntl.h>
 #undef _GNU_SOURCE /* revert for any other includes */
 
-#include "array.h"
 #include "greatest.h"
-#include "landlock.h"
 #include "seccomp.h"
 
-#include <arpa/inet.h> /* for inet_addr() */
+#include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <netdb.h>
-#include <stdio.h> /* for P_tmpdir */
 #include <sys/mman.h>
 #include <sys/socket.h>
 #include <unistd.h>
-
-TEST before_landlock_filesystem(void) {
-	int fd = open(__FILE__, O_RDONLY);
-	ASSERT_GTE(fd, 0);
-	int rc = close(fd);
-	ASSERT_EQ(rc, 0);
-
-	int tmpfd = open(P_tmpdir, O_TMPFILE | O_EXCL | O_RDWR, 0);
-	ASSERT_GTE(tmpfd, 0);
-	rc = close(tmpfd);
-	ASSERT_EQ(rc, 0);
-
-	PASS();
-}
-
-TEST before_landlock_network(void) {
-	struct addrinfo hints;
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-
-	int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	ASSERT_GTE(sfd, 0);
-
-	struct sockaddr_in sa;
-	memset(&sa, 0, sizeof(sa));
-	sa.sin_family = AF_INET;
-	sa.sin_port = htons(443);
-	inet_pton(AF_INET, "93.184.215.14", &sa.sin_addr); /* example.com */
-
-	int rc = connect(sfd, &sa, sizeof(sa));
-	ASSERT_GTE(rc, 0);
-
-	rc = close(sfd);
-	ASSERT_EQ(rc, 0);
-
-	PASS();
-}
-
-SUITE(before_landlock) {
-	RUN_TEST(before_landlock_filesystem);
-	RUN_TEST(before_landlock_network);
-}
-
-TEST setup_partial_landlock(void) {
-	const char *paths[] = {
-		P_tmpdir,
-	};
-	landlock_apply(paths, 1, 443);
-	PASS();
-}
-
-TEST partial_landlock_filesystem(void) {
-	int fd = open(__FILE__, O_RDONLY);
-	ASSERT_LT(fd, 0);
-
-	int tmpfd = open(P_tmpdir, O_TMPFILE | O_EXCL | O_RDWR, 0);
-	ASSERT_GTE(tmpfd, 0);
-	int rc = close(tmpfd);
-	ASSERT_EQ(rc, 0);
-
-	PASS();
-}
-
-SUITE(partial_landlock) {
-	RUN_TEST(setup_partial_landlock);
-	RUN_TEST(partial_landlock_filesystem);
-	RUN_TEST(before_landlock_network); /* reuse before_* network check */
-}
-
-TEST setup_full_landlock(void) {
-	landlock_apply(NULL, 0, 0);
-	PASS();
-}
-
-TEST after_landlock_filesystem(void) {
-	int fd = open(__FILE__, O_RDONLY);
-	ASSERT_LT(fd, 0);
-
-	int tmpfd = open(P_tmpdir, O_TMPFILE | O_EXCL | O_RDWR, 0);
-	ASSERT_LT(tmpfd, 0);
-
-	PASS();
-}
-
-TEST after_landlock_network(void) {
-	struct addrinfo hints;
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-
-	int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	ASSERT_GTE(sfd, 0);
-
-	struct sockaddr_in sa;
-	memset(&sa, 0, sizeof(sa));
-	sa.sin_family = AF_INET;
-	sa.sin_port = htons(443);
-	inet_pton(AF_INET, "93.184.215.14", &sa.sin_addr); /* example.com */
-
-	int rc = connect(sfd, &sa, sizeof(sa));
-	ASSERT_EQ(rc, -1);
-	ASSERT_EQ(errno, EACCES);
-
-	rc = close(sfd);
-	ASSERT_EQ(rc, 0);
-
-	PASS();
-}
-
-SUITE(full_landlock) {
-	RUN_TEST(setup_full_landlock);
-	RUN_TEST(after_landlock_filesystem);
-	RUN_TEST(after_landlock_network);
-}
 
 TEST before_seccomp(void) {
 	pid_t my_pid = getpid();
@@ -299,40 +180,14 @@ TEST seccomp_io(void) {
 	PASS();
 }
 
-TEST full_seccomp(void) {
-	seccomp_apply(SECCOMP_SANDBOX);
-
-	/* even getpid should be blocked now */
-	pid_t my_pid = getpid();
-	ASSERT_LT(my_pid, 0);
-	ASSERT_EQ(errno, EACCES);
-
-	/* mmap should no longer allow PROT_EXEC */
-	void *p = mmap(NULL, 4, PROT_EXEC, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-	ASSERT_EQ(p, MAP_FAILED);
-	ASSERT_EQ(errno, EACCES);
-	/* mmap should no longer allow PROT_READ */
-	p = mmap(NULL, 4, PROT_READ, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-	ASSERT_EQ(p, MAP_FAILED);
-	ASSERT_EQ(errno, EACCES);
-
-	/* socket should no longer be allowed */
-	int sfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	ASSERT_LT(sfd, 0);
-	ASSERT_EQ(errno, EACCES);
-
-	/* open should no longer allow O_RDONLY */
-	int fd = open(__FILE__, O_RDONLY);
-	ASSERT_LT(fd, 0);
-	ASSERT_EQ(errno, EACCES);
-
-	/* open should no longer allow O_TMPFILE */
-	int tmpfd = open(P_tmpdir, O_TMPFILE | O_EXCL | O_RDWR, 0);
-	ASSERT_LT(tmpfd, 0);
-	ASSERT_EQ(errno, EACCES);
-
-	PASS();
-}
+/*
+ * Note: it seems we cannot currently test seccomp_apply(SECCOMP_SANDBOX) in
+ * this single-process unit test rig, as restricting the sandbox to this degree
+ * prevents the unit test rig from proceeding, e.g. prevents greatest.h macros
+ * from being able to print test results to stdout. We'd probably need a
+ * multiprocess or multithreaded unit test rig, to allow seccomp_apply() to
+ * restrict a child process without restricting the unit test rig itself.
+ */
 
 SUITE(seccomp) {
 	RUN_TEST(before_seccomp);
@@ -340,7 +195,6 @@ SUITE(seccomp) {
 	RUN_TEST(seccomp_io_inet_rpath);
 	RUN_TEST(seccomp_io_inet);
 	RUN_TEST(seccomp_io);
-	RUN_TEST(full_seccomp);
 }
 
 GREATEST_MAIN_DEFS();
@@ -350,12 +204,6 @@ main(int argc, char **argv)
 {
 	GREATEST_MAIN_BEGIN();
 
-	/*
-	 * Note: these tests are similar to `youtube-unthrottle --try-sandbox`
-	 */
-	RUN_SUITE(before_landlock);
-	RUN_SUITE(partial_landlock);
-	RUN_SUITE(full_landlock);
 	RUN_SUITE(seccomp);
 
 	GREATEST_MAIN_END();
