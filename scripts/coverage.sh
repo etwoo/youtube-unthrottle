3da9253131f8f6496065602180a93f061be92e6b
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2024-08-19 03:36:50 -0700

    refactor coverage.sh, fix shellcheck errors
    Hoist constants in coverage.sh up into global variables, and fix
    shellcheck errors:
    
      *.profraw
      ^-- SC2035 (info): Use ./*glob* ... so names with dashes won't become options

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,9 +6,11 @@ set -euxo pipefail
 # TODO: change open_coverage_fd() to choose a unique filename per test; maybe treat COVERAGE_PROFILE like a directory, and generate a random/unique filename to create within that directory? then teach this script to process all the files within the COVERAGE_PROFILE directory
 COVERAGE_PROFILE=seccomp.profraw ./build/tests/sandbox/sandbox-seccomp -v
 
-llvm-profdata merge -sparse -o sandbox.profdata *.profraw
+BIN='./build/youtube-unthrottle'
+PROFILE_DATA='sandbox.profdata'
+LCOV_FMT='sandbox.lcov'
+LCOV_TO_COBERTURA=$(find ./build -name lcov_cobertura.py)
 
-llvm-cov export -instr-profile=sandbox.profdata ./build/youtube-unthrottle -format=lcov > sandbox.lcov
-
-LCOV_COBERTURA_PY=$(find ./build -name lcov_cobertura.py)
-python "$LCOV_COBERTURA_PY" sandbox.lcov -o sandbox.xml
+llvm-profdata merge -sparse -o "$PROFILE_DATA" ./*.profraw
+llvm-cov export -instr-profile="$PROFILE_DATA" "$BIN" -format=lcov > "$LCOV_FMT"
+python "$LCOV_TO_COBERTURA" "$LCOV_FMT" -o sandbox.xml
