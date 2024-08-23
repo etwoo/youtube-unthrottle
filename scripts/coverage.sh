e7814c01a35f610093229f163a07059e2a123c5c
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2024-08-22 23:17:08 -0700

    try to add coverage percentage to MR widget

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -11,5 +11,22 @@ LCOV_TO_COBERTURA=$(find ./build -name lcov_cobertura.py)
 
 find ./build -path "*/$COVERAGE_PROFILE_DIR/*" | \
 	xargs llvm-profdata merge -sparse -o "$PROFILE_DATA"
+
+#
+# Generate a coverage report that includes a single-value line coverage
+# percentage, which GitLab can then display in the merge request widget.
+#
+# https://docs.gitlab.com/ee/ci/testing/code_coverage.html#view-code-coverage-results-in-the-merge-request
+#
+llvm-cov report -show-region-summary=0 -show-branch-summary=0 \
+	-instr-profile="$PROFILE_DATA" "$BIN" \
+	-ignore-filename-regex 'main.c|src/coverage.c'
+
+#
+# Convert profdata -> lcov -> cobertura. GitLab the latter to drive the test
+# coverage visualization feature in the file diff view of MRs.
+#
+# https://docs.gitlab.com/ee/ci/testing/test_coverage_visualization/index.html
+#
 llvm-cov export -instr-profile="$PROFILE_DATA" "$BIN" -format=lcov > "$LCOV_FMT"
 python "$LCOV_TO_COBERTURA" "$LCOV_FMT" -o "$COBERTURA_OUTPUT"
