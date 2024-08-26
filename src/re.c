3a3f6823bb34f52abe45ebadf88c88632eec70cc
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2024-08-26 16:29:46 -0700

    re_capture: `goto cleanup` -> RAII
    Refactor re_capture() from `goto cleanup` style to RAII style, using
    __attribute__((cleanup)).

--- src/re.c
+++ src/re.c
@@ -10,64 +10,24 @@
 #define PCRE2_CODE_UNIT_WIDTH 8
 #include <pcre2.h>
 
-bool
-re_capture(const char *pattern_in,
-           const char *subject_in,
-           size_t sz,
-           const char **capture_p,
-           size_t *capture_sz)
+static void
+re_cleanup(pcre2_code **re)
 {
-	bool matched = false;
-	const char *action_that_caused_error = "";
-	pcre2_code *re = NULL;
-	pcre2_match_data *md = NULL;
-	PCRE2_SPTR pattern = (PCRE2_SPTR)pattern_in;
-	PCRE2_SPTR subject = (PCRE2_SPTR)subject_in;
-	int rc = 0;
-	PCRE2_SIZE loc = 0;
+	pcre2_code_free(*re); /* handles NULL gracefully */
+}
 
-	re = pcre2_compile(pattern, PCRE2_ZERO_TERMINATED, 0, &rc, &loc, NULL);
-	if (re == NULL) {
-		action_that_caused_error = "compiling";
-		goto cleanup;
-	}
+static void
+re_match_cleanup(pcre2_match_data **md)
+{
+	pcre2_match_data_free(*md); /* handles NULL gracefully */
+}
 
-	md = pcre2_match_data_create_from_pattern(re, NULL);
-	if (md == NULL) {
-		action_that_caused_error = "creating match structure for";
-		goto cleanup;
-	}
-
-	rc = pcre2_match(re, subject, sz, 0, 0, md, NULL);
-	if (rc > 0) {
-		matched = true;
-		assert(rc == 2); /* pattern_in must contain one capture group */
-	} else {
-		if (rc != PCRE2_ERROR_NOMATCH) {
-			action_that_caused_error = "matching";
-		} /* else: *_NOMATCH is a non-error for our purposes */
-		goto cleanup;
-	}
-
-	PCRE2_SIZE *ovector = pcre2_get_ovector_pointer(md);
-	assert(ovector != NULL); /* md!=NULL should guarantee ovector!=NULL */
-
-	debug("Regex \"%s\" matched at offset %zd within subject of size %zd",
-	      pattern_in,
-	      (size_t)ovector[0],
-	      sz);
-
-	*capture_p = subject_in + ovector[2];
-	*capture_sz = ovector[3] - ovector[2];
-
-cleanup:
-	pcre2_match_data_free(md); /* handles NULL gracefully */
-	pcre2_code_free(re);       /* handles NULL gracefully */
-
-	if (0 == strlen(action_that_caused_error)) {
-		return matched;
-	} /* else: fallthrough to error handler */
-
+static void
+re_info_message(const char *action_that_caused_error,
+                int rc,
+                const char *pattern_in,
+                PCRE2_SIZE loc)
+{
 	PCRE2_UCHAR err[256];
 	if (pcre2_get_error_message(rc, err, sizeof(err)) < 0) {
 		info("Error (no details) while %s regex \"%s\"",
@@ -80,7 +40,54 @@ cleanup:
 		     (size_t)loc,
 		     err);
 	}
-	return false;
+}
+
+bool
+re_capture(const char *pattern_in,
+           const char *subject_in,
+           size_t sz,
+           const char **capture_p,
+           size_t *capture_sz)
+{
+	int rc = 0;
+	PCRE2_SIZE loc = 0;
+
+	PCRE2_SPTR pat = (PCRE2_SPTR)pattern_in;
+	pcre2_code *re __attribute__((cleanup(re_cleanup))) =
+		pcre2_compile(pat, PCRE2_ZERO_TERMINATED, 0, &rc, &loc, NULL);
+	if (re == NULL) {
+		re_info_message("compiling", rc, pattern_in, loc);
+		return false;
+	}
+
+	pcre2_match_data *md __attribute__((cleanup(re_match_cleanup))) =
+		pcre2_match_data_create_from_pattern(re, NULL);
+	if (md == NULL) {
+		re_info_message("allocating match for", rc, pattern_in, loc);
+		return false;
+	}
+
+	rc = pcre2_match(re, (PCRE2_SPTR)subject_in, sz, 0, 0, md, NULL);
+	if (rc > 0) {
+		assert(rc == 2); /* pattern_in must contain one capture group */
+	} else {
+		if (rc != PCRE2_ERROR_NOMATCH) {
+			re_info_message("matching", rc, pattern_in, loc);
+		} /* else: *_NOMATCH is a non-error for our purposes */
+		return false;
+	}
+
+	PCRE2_SIZE *ovector = pcre2_get_ovector_pointer(md);
+	assert(ovector != NULL); /* md!=NULL should guarantee ovector!=NULL */
+
+	debug("Regex \"%s\" matched at offset %zd within subject of size %zd",
+	      pattern_in,
+	      (size_t)ovector[0],
+	      sz);
+
+	*capture_p = subject_in + ovector[2];
+	*capture_sz = ovector[3] - ovector[2];
+	return true;
 }
 
 bool
