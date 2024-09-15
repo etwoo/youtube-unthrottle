c9175c2323ba0f783c2417001209fc7bddb1ed50
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2024-09-15 16:46:23 -0700

    add tests for youtube URL parsing edge cases
    Cover additional edge cases around youtube URLs:
    
    1) target URL passed to youtube_stream_setup() lacks a `v=...` stream ID
    2) n-parameter is the first GET argument
    3) n-parameter is the last GET argument
    4) n-parameter is a middle GET argument
    5) n-parameter is missing from GET arguments
    6) n-parameter has an empty value
    6) video/audio stream URLs are missing from the adaptiveFormats array

--- tests/youtube/youtube.c
+++ tests/youtube/youtube.c
@@ -10,6 +10,7 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+static const char PATH_WANTS_JSON_RESPONSE[] = "/youtubei/v1/player";
 static const char FAKE_YT_URL[] = "https://www.youtube.com/watch?v=FOOBAR";
 static const char FAKE_HTML_RESPONSE[] = "\"/s/player/foobar/base.js\"";
 static const char FAKE_JSON_RESPONSE[] =
@@ -22,17 +23,25 @@ static const char FAKE_JS_RESPONSE[] =
 	"&&(c=X[0](c),\nvar X=[Y];\nY=function(a)"
 	"{b=[a.toUpperCase()]; return b.join(\"\")};";
 
+static const char *(*test_request_path_to_response)(const char *) = NULL;
+
 static int
 test_fixture_request_handler(void *request, const char *path, int fd)
 {
 	debug("Mocking request: CURL* %p, %s, fd=%d", request, path, fd);
 
 	const char *to_write = NULL;
-	if (0 == strlen(path)) {
+	if (test_request_path_to_response) {
+		to_write = test_request_path_to_response(path);
+	}
+
+	if (to_write) {
+		/* got a custom value from test-specific handler */
+	} else if (0 == strlen(path)) {
 		to_write = ""; /* handle thread warmup in url_global_init() */
 	} else if (strstr(path, "/watch?v=")) {
 		to_write = FAKE_HTML_RESPONSE;
-	} else if (strstr(path, "/youtubei/v1/player")) {
+	} else if (strstr(path, PATH_WANTS_JSON_RESPONSE)) {
 		to_write = FAKE_JSON_RESPONSE;
 	} else if (strstr(path, "/base.js")) {
 		to_write = FAKE_JS_RESPONSE;
@@ -42,7 +51,6 @@ test_fixture_request_handler(void *request, const char *path, int fd)
 
 	ssize_t written = write_with_retry(fd, to_write, strlen(to_write));
 	info_m_if(written < 0, "Cannot write to tmpfile");
-
 	return 0;
 }
 
@@ -63,17 +71,19 @@ struct youtube_setup_ops NOOP = {
 	.after = setup_callback_noop,
 };
 
-static bool CHECK_URL_RESULT = true;
+static bool GOT_CORRECT_URLS = true;
+static const char *EXPECTED_AUDIO_URL = NULL;
+static const char *EXPECTED_VIDEO_URL = NULL;
 
 static void
 check_url(const char *url)
 {
-	if (0 == strcmp("http://a.test/?n=AAA", url)) {
+	if (0 == strcmp(EXPECTED_AUDIO_URL, url)) {
 		debug("Got expected audio URL: %s", url);
-	} else if (0 == strcmp("http://v.test/?n=VVV", url)) {
+	} else if (0 == strcmp(EXPECTED_VIDEO_URL, url)) {
 		debug("Got expected video URL: %s", url);
 	} else {
-		CHECK_URL_RESULT = false;
+		GOT_CORRECT_URLS = false;
 		info("check_url() fails: %s", url);
 	}
 }
@@ -87,17 +97,30 @@ global_setup(void)
 }
 
 TEST
-stream_setup_with_redirected_network_io(void)
+stream_setup_with_redirected_network_io(const char *(*custom_fn)(const char *),
+                                        const char *expected_audio_url,
+                                        const char *expected_video_url)
 {
 	youtube_handle_t stream = youtube_stream_init();
 	ASSERT(stream);
 
+	test_request_path_to_response = custom_fn;
 	result_t err = youtube_stream_setup(stream, &NOOP, FAKE_YT_URL);
+	test_request_path_to_response = NULL;
+
 	ASSERT_EQ(err.err, OK);
 
+	GOT_CORRECT_URLS = true;
+	EXPECTED_AUDIO_URL = expected_audio_url;
+	EXPECTED_VIDEO_URL = expected_video_url;
+
 	err = youtube_stream_visitor(stream, check_url);
+
+	EXPECTED_AUDIO_URL = NULL;
+	EXPECTED_VIDEO_URL = NULL;
+
 	ASSERT_EQ(err.err, OK);
-	ASSERT(CHECK_URL_RESULT);
+	ASSERT(GOT_CORRECT_URLS);
 
 	youtube_stream_cleanup(stream);
 	PASS();
@@ -127,20 +150,158 @@ stream_setup_with_null_ops(void)
 	PASS();
 }
 
-TEST
-global_cleanup(void)
-{
-	youtube_global_cleanup();
-	PASS();
-}
-
-SUITE(stream_setup)
+SUITE(stream_setup_simple)
 {
 	url_global_set_request_handler(test_fixture_request_handler);
 	RUN_TEST(global_setup);
-	RUN_TEST(stream_setup_with_redirected_network_io);
+	RUN_TESTp(stream_setup_with_redirected_network_io,
+	          NULL,
+	          "http://a.test/?n=AAA",
+	          "http://v.test/?n=VVV");
 	RUN_TEST(stream_setup_with_null_ops);
-	RUN_TEST(global_cleanup);
+}
+
+static const char YT_URL_MISSING_ID[] = "https://www.youtube.com/watch?v=";
+
+TEST
+stream_setup_edge_cases_target_url_missing_stream_id(void)
+{
+	youtube_handle_t stream = youtube_stream_init();
+	ASSERT(stream);
+
+	result_t err = youtube_stream_setup(stream, &NOOP, YT_URL_MISSING_ID);
+	ASSERT_EQ(err.err, ERR_YOUTUBE_INNERTUBE_POST_ID);
+
+	youtube_stream_cleanup(stream);
+	PASS();
+}
+
+SUITE(stream_setup_target_url_variations)
+{
+	url_global_set_request_handler(test_fixture_request_handler);
+	RUN_TEST(global_setup);
+	RUN_TEST(stream_setup_edge_cases_target_url_missing_stream_id);
+}
+
+static const char *
+test_request_n_param_pos_middle(const char *path)
+{
+	if (NULL == strstr(path, PATH_WANTS_JSON_RESPONSE)) {
+		return NULL;
+	}
+	return "{\"streamingData\": {\"adaptiveFormats\": ["
+	       "{\"mimeType\": \"audio/foobar\",\"url\": \""
+	       "http://a.test?first=foo&n=aaa&last=bar"
+	       "\"},"
+	       "{\"mimeType\": \"video/foobar\",\"url\": \""
+	       "http://v.test?first=foo&n=vvv&last=bar"
+	       "\"}"
+	       "]}}";
+}
+
+static const char *
+test_request_n_param_pos_first(const char *path)
+{
+	if (NULL == strstr(path, PATH_WANTS_JSON_RESPONSE)) {
+		return NULL;
+	}
+	return "{\"streamingData\": {\"adaptiveFormats\": ["
+	       "{\"mimeType\": \"audio/foobar\",\"url\": \""
+	       "http://a.test?n=aaa&second=foo&third=bar"
+	       "\"},"
+	       "{\"mimeType\": \"video/foobar\",\"url\": \""
+	       "http://v.test?n=vvv&second=foo&third=bar"
+	       "\"}"
+	       "]}}";
+}
+
+static const char *
+test_request_n_param_pos_last(const char *path)
+{
+	if (NULL == strstr(path, PATH_WANTS_JSON_RESPONSE)) {
+		return NULL;
+	}
+	return "{\"streamingData\": {\"adaptiveFormats\": ["
+	       "{\"mimeType\": \"audio/foobar\",\"url\": \""
+	       "http://a.test?first=foo&second=bar&n=aaa"
+	       "\"},"
+	       "{\"mimeType\": \"video/foobar\",\"url\": \""
+	       "http://v.test?first=foo&second=bar&n=vvv"
+	       "\"}"
+	       "]}}";
+}
+
+static const char *
+test_request_n_param_empty_or_missing(const char *path)
+{
+	if (NULL == strstr(path, PATH_WANTS_JSON_RESPONSE)) {
+		return NULL;
+	}
+	return "{\"streamingData\": {\"adaptiveFormats\": ["
+	       "{\"mimeType\": \"audio/foobar\",\"url\": \"http://a.test?n=\"},"
+	       "{\"mimeType\": \"video/foobar\",\"url\": \"http://v.test?x=y\"}"
+	       "]}}";
+}
+
+TEST
+stream_setup_edge_cases_n_param_missing(void)
+{
+	youtube_handle_t stream = youtube_stream_init();
+	ASSERT(stream);
+
+	test_request_path_to_response = test_request_n_param_empty_or_missing;
+	result_t err = youtube_stream_setup(stream, &NULL_OPS, FAKE_YT_URL);
+	test_request_path_to_response = NULL;
+
+	ASSERT_EQ(err.err, ERR_YOUTUBE_N_PARAM_FIND_IN_QUERY);
+
+	youtube_stream_cleanup(stream);
+	PASS();
+}
+
+static const char *
+test_request_entire_url_missing(const char *path)
+{
+	if (NULL == strstr(path, PATH_WANTS_JSON_RESPONSE)) {
+		return NULL;
+	}
+	return "{\"streamingData\": {\"adaptiveFormats\": []}}";
+}
+
+TEST
+stream_setup_edge_cases_entire_url_missing(void)
+{
+	youtube_handle_t stream = youtube_stream_init();
+	ASSERT(stream);
+
+	test_request_path_to_response = test_request_entire_url_missing;
+	result_t err = youtube_stream_setup(stream, &NULL_OPS, FAKE_YT_URL);
+	test_request_path_to_response = NULL;
+
+	ASSERT_EQ(err.err, ERR_YOUTUBE_N_PARAM_QUERY_GET);
+
+	youtube_stream_cleanup(stream);
+	PASS();
+}
+
+SUITE(stream_setup_n_param_positions)
+{
+	url_global_set_request_handler(test_fixture_request_handler);
+	RUN_TEST(global_setup);
+	RUN_TESTp(stream_setup_with_redirected_network_io,
+	          test_request_n_param_pos_middle,
+	          "http://a.test/?first=foo&last=bar&n=AAA",
+	          "http://v.test/?first=foo&last=bar&n=VVV");
+	RUN_TESTp(stream_setup_with_redirected_network_io,
+	          test_request_n_param_pos_first,
+	          "http://a.test/?second=foo&third=bar&n=AAA",
+	          "http://v.test/?second=foo&third=bar&n=VVV");
+	RUN_TESTp(stream_setup_with_redirected_network_io,
+	          test_request_n_param_pos_last,
+	          "http://a.test/?first=foo&second=bar&n=AAA",
+	          "http://v.test/?first=foo&second=bar&n=VVV");
+	RUN_TEST(stream_setup_edge_cases_n_param_missing);
+	RUN_TEST(stream_setup_edge_cases_entire_url_missing);
 }
 
 GREATEST_MAIN_DEFS();
@@ -152,7 +313,10 @@ main(int argc, char **argv)
 
 	GREATEST_MAIN_BEGIN();
 
-	RUN_SUITE(stream_setup);
+	RUN_SUITE(stream_setup_simple);
+	RUN_SUITE(stream_setup_target_url_variations);
+	RUN_SUITE(stream_setup_n_param_positions);
 
+	youtube_global_cleanup();
 	GREATEST_MAIN_END();
 }
