2773ba13810c92f5177a9e74623276179bf988f1
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2024-12-12 08:19:18 +0000

    consolidate repetitive CMakeLists.txt boilerplate

--- cmake/CheckCodeCoverage.cmake
+++ cmake/CheckCodeCoverage.cmake
@@ -3,21 +3,37 @@ include_guard(GLOBAL)
 include(CMakePushCheckState)
 include(CheckCCompilerFlag)
 
-function(check_code_coverage flags)
-	check_c_compiler_flag(-fprofile-instr-generate CFLAG_PROFILE_INSTR_GEN)
+macro(my_msg str)
+	message(STATUS "Detecting code coverage support" ${str})
+endmacro()
 
+macro(check_code_coverage lib)
+	my_msg("")
+	cmake_push_check_state()
+
+	set(CMAKE_REQUIRED_QUIET ON)
+	check_c_compiler_flag(-fprofile-instr-generate CFLAG_PROFILE_INSTR_GEN)
 	if (NOT CFLAG_PROFILE_INSTR_GEN)
+		my_msg(" - Failed")
 		message(FATAL_ERROR "No support for -fprofile-instr-generate")
 	endif (NOT CFLAG_PROFILE_INSTR_GEN)
 
-	cmake_push_check_state()
 	set(CMAKE_REQUIRED_FLAGS -fprofile-instr-generate)
 	check_c_compiler_flag(-fcoverage-mapping CFLAG_COVERAGE_MAPPING)
-	cmake_pop_check_state()
-
 	if (NOT CFLAG_COVERAGE_MAPPING)
+		my_msg(" - Failed")
 		message(FATAL_ERROR "No support for -fcoverage-mapping")
 	endif (NOT CFLAG_COVERAGE_MAPPING)
 
-	set(${flags} "-fprofile-instr-generate;-fcoverage-mapping" PARENT_SCOPE)
-endfunction()
+	cmake_pop_check_state()
+	my_msg(" - Success")
+
+	target_compile_definitions(${lib} INTERFACE WITH_COVERAGE)
+
+	target_compile_options(${lib} INTERFACE
+		-fprofile-instr-generate -fcoverage-mapping
+	)
+	target_link_options(${lib} INTERFACE
+		-fprofile-instr-generate -fcoverage-mapping
+	)
+endmacro()
