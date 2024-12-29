5714d12b31f5dfbd7875f42f52e7618ec994b0df
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2024-12-28 22:15:32 -0800

    adjust static duration check

--- scripts/check-global-variables.sh
+++ scripts/check-global-variables.sh
@@ -17,8 +17,11 @@ fi
 
 echo 'Checking for non-const local variables with static duration ...'
 if find . -type f -regextype egrep -regex '.*\.(h|c)' -and ! -path './build/*' \
-	| ctags -L- -x --sort=yes --c-kinds=l \
-	| grep static ; then
+	| ctags -L- --output-format=json --sort=yes --c-kinds=l \
+	| jq -rM .pattern \
+	| sed 's@^/\^@@g' \
+	| sed 's@\$/$@@g' \
+	| grep -P '\t+static' ; then
 	echo 'ERROR: non-const variables with static duration make this code non-reentrant and likely not thread-safe; please refactor this code to avoid mutable global state'
 	STATUS=1
 fi
