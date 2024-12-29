1b56841b243e3addf7228d182d7f12096d1543b8
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2024-12-28 16:35:17 -0800

    add missing newline to printf() output
    Accidentally dropped newline when converting from puts to printf.

--- main.c
+++ main.c
@@ -154,7 +154,7 @@ during_parse_choose_quality(const char *val, void *userdata)
 static void
 print_url(const char *url, size_t sz, void *userdata __attribute((unused)))
 {
-	printf("%.*s", (int)sz, url);
+	printf("%.*s\n", (int)sz, url);
 }
 
 static __attribute__((warn_unused_result)) result_t
