28b72ec80330edb92339ce5105ccaac8187f52f2
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-01-03 21:48:34 -0800

    dedup copy-paste code in check-global-variables.sh

--- scripts/check-global-variables.sh
+++ scripts/check-global-variables.sh
@@ -2,28 +2,37 @@
 
 set -euo pipefail
 
+function find_c_code
+{
+	find . -regextype egrep -regex '.*\.(h|c)' -and ! -path './build/*' \
+		| ctags -L- --output-format=json $@ \
+		| jq -rM '.pattern | ltrimstr("/^") | rtrimstr("$/")'
+}
+
 STATUS=0
 
 echo 'Checking for non-const global variables ...'
-if find . -type f -regextype egrep -regex '.*\.(h|c)' -and ! -path './build/*' \
-	| ctags -L- --output-format=json --c-kinds=v --extras=-F \
-	| jq -rM .pattern \
-	| sed 's@^/\^@@g' \
-	| sed 's@\$/$@@g' \
-	| grep -v '^const' ; then
-	echo 'ERROR: non-const global variables potentially make this code not thread-safe; please refactor this code to avoid mutable global state'
+if find_c_code --c-kinds=v --extras=-F | grep -v '^const' ; then
+	cat << EOF
+ERROR: non-const global variables potentially make this code not thread-safe
+
+EOF
 	STATUS=1
 fi
 
 echo 'Checking for non-const local variables with static duration ...'
-if find . -type f -regextype egrep -regex '.*\.(h|c)' -and ! -path './build/*' \
-	| ctags -L- --output-format=json --c-kinds=l \
-	| jq -rM .pattern \
-	| sed 's@^/\^@@g' \
-	| sed 's@\$/$@@g' \
-	| grep -P '\t+static' ; then
-	echo 'ERROR: non-const variables with static duration make this code non-reentrant and likely not thread-safe; please refactor this code to avoid mutable global state'
+if find_c_code --c-kinds=l | grep -P '\t+static' ; then
+	cat << EOF
+ERROR: non-const variables with static duration make this code non-reentrant
+
+EOF
 	STATUS=1
 fi
 
+if [ $STATUS -ne 0 ]; then
+	echo 'Please refactor this code to avoid mutable global state!'
+
+fi
+
+echo "Exiting with status $STATUS"
 exit $STATUS
