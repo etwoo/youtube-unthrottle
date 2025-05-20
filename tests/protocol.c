6ddff9c0cfdc39c900361eee5020bf4662a58d1f
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-05-20 16:19:36 -0700

    finish conversion to ump_varint_read() usage

--- tests/protocol.c
+++ tests/protocol.c
@@ -2,63 +2,38 @@
 #include "protocol/stream.h"
 
 static enum greatest_test_res
-test_ump_varint_read(char byte_to_parse, uint64_t expected)
+test_ump_varint_read_n(size_t n, char *bytes_to_parse, uint64_t expected)
 {
-	char buf[2] = { byte_to_parse, 0 };
 	const struct string_view view = {
-		.data = buf,
-		.sz = sizeof(buf),
+		.data = bytes_to_parse,
+		.sz = n + 1,
 	};
 	size_t pos = 0;
 	uint64_t actual = 0;
 
 	auto_result err = ump_varint_read(&view, &pos, &actual);
 	ASSERT_EQ(OK, err.err);
-	ASSERT_EQ(1, pos);
+	ASSERT_EQ(n, pos);
 	ASSERT_EQ(expected, actual);
 
 	PASS();
 }
 
 TEST
-protocol_ump_read_vle_byte_length_one(void)
+protocol_ump_read_vle(void)
 {
-	CHECK_CALL(test_ump_varint_read(0x00, 0));
-#if 0
-	ump_read_vle(0x81, &bytes_to_read, &first_byte_mask);
-	ASSERT_EQ(2, bytes_to_read);
-	ASSERT_EQ(0x3F, first_byte_mask);
-	ASSERT_EQ(1, 0x81 & first_byte_mask);
-
-	ump_read_vle(0x88, &bytes_to_read, &first_byte_mask);
-	ASSERT_EQ(2, bytes_to_read);
-	ASSERT_EQ(0x3F, first_byte_mask);
-	ASSERT_EQ(8, 0x88 & first_byte_mask);
-
-	ump_read_vle(0x8F, &bytes_to_read, &first_byte_mask);
-	ASSERT_EQ(2, bytes_to_read);
-	ASSERT_EQ(0x3F, first_byte_mask);
-	ASSERT_EQ(15, 0x8F & first_byte_mask);
-
-	ump_read_vle(0xA0, &bytes_to_read, &first_byte_mask);
-	ASSERT_EQ(2, bytes_to_read);
-	ASSERT_EQ(0x3F, first_byte_mask);
-	ASSERT_EQ(32, 0xA0 & first_byte_mask);
-
-	ump_read_vle(0xAA, &bytes_to_read, &first_byte_mask);
-	ASSERT_EQ(2, bytes_to_read);
-	ASSERT_EQ(0x3F, first_byte_mask);
-	ASSERT_EQ(42, 0xAA & first_byte_mask);
-
-	ump_read_vle(0xBF, &bytes_to_read, &first_byte_mask);
-	ASSERT_EQ(2, bytes_to_read);
-	ASSERT_EQ(0x3F, first_byte_mask);
-	ASSERT_EQ(63, 0xBF & first_byte_mask);
-#endif
+	CHECK_CALL(test_ump_varint_read_n(1, (char[2]){0x00, 0x00}, 0));
+	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0x80, 0x01, 0x00}, 64));
+	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0x81, 0x01, 0x00}, 65));
+	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0x88, 0x01, 0x00}, 72));
+	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0x8F, 0x01, 0x00}, 79));
+	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0xA0, 0x01, 0x00}, 96));
+	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0xAA, 0x01, 0x00}, 106));
+	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0xBF, 0x01, 0x00}, 127));
 	PASS();
 }
 
 SUITE(protocol_ump)
 {
-	RUN_TEST(protocol_ump_read_vle_byte_length_one);
+	RUN_TEST(protocol_ump_read_vle);
 }
