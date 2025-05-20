6f1e12ef3a96b29250e200b3601e69481dbd8178
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-05-20 16:50:10 -0700

    add testcases for bytes_to_read=3

--- tests/protocol.c
+++ tests/protocol.c
@@ -13,6 +13,9 @@ test_ump_varint_read_n(size_t n, char *bytes_to_parse, uint64_t expected)
 	uint64_t actual = 0;
 
 	auto_result err = ump_varint_read(&view, &pos, &actual);
+	debug("Comparing expected %" PRIu64 " vs actual %" PRIu64,
+	      expected,
+	      actual);
 	ASSERT_EQ(OK, err.err);
 	ASSERT_EQ(n, pos);
 	ASSERT_EQ(expected, actual);
@@ -32,6 +35,12 @@ protocol_ump_read_vle(void)
 	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0xBF, 0x08, 0}, 575));
 	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0xBF, 0x10, 0}, 1087));
 	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0xBF, 0x7F, 0}, 8191));
+	CHECK_CALL(test_ump_varint_read_n(2, (char[3]){0xBF, 0xFF, 0}, 16383));
+	CHECK_CALL(test_ump_varint_read_n(3, (char[4]){0xDF, 0xFF, 0x01, 0}, 16383));
+	CHECK_CALL(test_ump_varint_read_n(3, (char[4]){0xDF, 0xFF, 0x08, 0}, 73727));
+	CHECK_CALL(test_ump_varint_read_n(3, (char[4]){0xDF, 0xFF, 0x10, 0}, 139263));
+	CHECK_CALL(test_ump_varint_read_n(3, (char[4]){0xDF, 0xFF, 0x7F, 0}, 1048575));
+	// CHECK_CALL(test_ump_varint_read_n(3, (char[4]){0xDF, 0xFF, 0xFF, 0}, 2097151));
 	PASS();
 }
 
