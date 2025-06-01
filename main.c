039a40680c3a3d1e810a1f535bb9131002b15985
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-06-01 00:59:27 -0700

    SO_SNDBUF: ask OS to hold more in-flight TCP data

--- main.c
+++ main.c
@@ -104,6 +104,7 @@ get_output_fd(in_port_t port, int *out, size_t out_sz)
 
 	int rc = -1;
 	const int on = 1;
+	const int maxbuf = 268435456; /* 256MB */
 
 	rc = setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 	if (rc < 0) {
@@ -115,6 +116,13 @@ get_output_fd(in_port_t port, int *out, size_t out_sz)
 	rc = setsockopt(sfd, SOL_SOCKET, SO_REUSEPORT, &on, sizeof(on));
 	if (rc < 0) {
 		to_stderr("Can't set SO_REUSEPORT for %d: %s",
+		          port, strerror(errno));
+		goto cleanup;
+	}
+
+	rc = setsockopt(sfd, SOL_SOCKET, SO_SNDBUF, &maxbuf, sizeof(maxbuf));
+	if (rc < 0) {
+		to_stderr("Can't set SO_SNDBUF for %d: %s",
 		          port, strerror(errno));
 		goto cleanup;
 	}
