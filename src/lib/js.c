2e5b2df89de59c9cf90d35a222215492f5f7c99a
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-06-02 02:19:10 -0700

    simplify and comment regex

--- src/lib/js.c
+++ src/lib/js.c
@@ -175,9 +175,11 @@ result_t
 find_itag_video(const struct string_view *json, long long int *value)
 {
 	struct string_view itag = {0};
-	check(re_capture("[\\n\\r][ ]{8}\"itag\": ([0-9]+),"
-	                 "(?:[\\n\\r][ ]{8,10}(?:},|\"[a-zA-Z]+\": .*,?))*"
-	                 "[\\n\\r][ ]{8}\"quality\": \"hd1080\"",
+	check(re_capture("\\n[ ]{8}\"itag\": ([0-9]+),"
+	                 "(?:" /* non-capturing group: (?:...) */
+	                 "\\n[ ]{8,10}(?:}|\"[a-zA-Z]+\": .+),?"
+	                 ")*" /* zero or more matches of non-capturing group */
+	                 "\\n[ ]{8}\"quality\": \"hd1080\"",
 	                 json,
 	                 &itag));
 	if (itag.data == NULL) {
