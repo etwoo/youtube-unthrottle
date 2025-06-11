fa9369f4af149d8388dcf3077a50ef5edb3464c2
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-06-10 19:40:38 -0700

    pin gcc-14 to workaround gcc-15 on ubuntu errors

--- scripts/homebrew-envvars.sh
+++ scripts/homebrew-envvars.sh
@@ -9,13 +9,6 @@ echo "CFLAGS=-I$BREW_PREFIX/include"
 echo "LDFLAGS=-L$BREW_PREFIX/lib"
 echo "LD_LIBRARY_PATH=$BREW_PREFIX/lib"
 
-# TODO: verify that as --version == 2.44, i.e. newest binutils
-echo "Checking assembler version ..."
-which as >&2
-as --version >&2
-ls $BREW_PREFIX/bin/*as >&2
-$BREW_PREFIX/bin/gas --version >&2
-
 # For reference on this idiom for checking if a glob matched, see:
 #   https://unix.stackexchange.com/a/298302
 shopt -s nullglob
