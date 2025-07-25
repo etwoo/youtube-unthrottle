6bcdf21149b8bd55675332b80c316f3dcf6a91c3
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-07-24 17:18:27 -0700

    adjust sandbox test name given assert() is (mostly) removed from sandbox_verify()

--- tests/sandbox.c
+++ tests/sandbox.c
@@ -3,7 +3,7 @@
 #include "greatest.h"
 
 TEST
-sandbox_ok_does_not_crash(void)
+sandbox_ok(void)
 {
 	sandbox_handle_t context = sandbox_init();
 	ASSERT_NEQ(NULL, context);
@@ -16,5 +16,5 @@ sandbox_ok_does_not_crash(void)
 
 SUITE(sandbox_smoke_test)
 {
-	RUN_TEST(sandbox_ok_does_not_crash);
+	RUN_TEST(sandbox_ok);
 }
