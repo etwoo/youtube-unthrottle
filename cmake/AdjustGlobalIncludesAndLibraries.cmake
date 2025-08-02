b14a8944feb3ebd7f522162874767587fa1ff4f1
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-08-02 05:43:39 -0700

    try to make homebrew logic more generic so that it works on Linux and Intel macOS

--- cmake/AdjustGlobalIncludesAndLibraries.cmake
+++ cmake/AdjustGlobalIncludesAndLibraries.cmake
@@ -1,15 +1,21 @@
 include_guard(GLOBAL)
 
 macro(adjust_global_includes_and_libraries)
-	if (CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-		if (IS_DIRECTORY /opt/homebrew)
-			message(STATUS "Using libraries installed via Homebrew")
-			include_directories(SYSTEM /opt/homebrew/include)
-			link_directories(/opt/homebrew/lib)
-		endif (IS_DIRECTORY /opt/homebrew)
-	elseif (CMAKE_SYSTEM_NAME STREQUAL "OpenBSD")
+	if (CMAKE_SYSTEM_NAME STREQUAL "OpenBSD")
 		message(STATUS "Using libraries installed via OpenBSD ports")
 		include_directories(SYSTEM /usr/local/include)
 		link_directories(/usr/local/lib)
-	endif ()
+	else (CMAKE_SYSTEM_NAME STREQUAL "OpenBSD")
+		find_program(BREW_COMMAND brew)
+		if (BREW_COMMAND)
+			execute_process(
+				COMMAND ${BREW_COMMAND} --prefix
+				OUTPUT_VARIABLE BREW_DIR
+				OUTPUT_STRIP_TRAILING_WHITESPACE
+			)
+			message(STATUS "Using Homebrew packages: ${BREW_DIR}")
+			include_directories(SYSTEM ${BREW_DIR}/include)
+			link_directories(${BREW_DIR}/lib)
+		endif (BREW_COMMAND)
+	endif (CMAKE_SYSTEM_NAME STREQUAL "OpenBSD")
 endmacro()
