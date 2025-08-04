c63d1b10795a9becb643bad1c75cc6a3d08c2e43
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-08-04 03:11:11 -0700

    remove unused IMPORTED_TARGET

--- cmake/TargetLinkPkgConfig.cmake
+++ cmake/TargetLinkPkgConfig.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 find_package(PkgConfig)
 
 function(target_link_from_pkg_config target pkg)
-	pkg_check_modules(MYPKG REQUIRED IMPORTED_TARGET ${pkg})
+	pkg_check_modules(MYPKG REQUIRED ${pkg})
 	target_include_directories(${target} PRIVATE ${MYPKG_INCLUDE_DIRS})
 	target_link_libraries(${target} PRIVATE ${MYPKG_LINK_LIBRARIES})
 endfunction()
