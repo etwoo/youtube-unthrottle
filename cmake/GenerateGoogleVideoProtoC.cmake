e1cebabbcdf45d074a6b59b9e78917c4c7377c25
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-08-12 23:38:30 -0700

    use protobuf_generate(), rm hand-rolled cmake

--- cmake/GenerateGoogleVideoProtoC.cmake
+++ cmake/GenerateGoogleVideoProtoC.cmake
@@ -1,34 +1,24 @@
 include_guard(GLOBAL)
 
+find_package(Protobuf REQUIRED)
+
 macro(generate_googlevideo_protoc target)
 	target_link_libraries(${target} PRIVATE
 		protobuf-c
 	)
 
 	set(PROTO_PREFIX "${googlevideo_SOURCE_DIR}/protos")
-	file(GLOB PROTOS RELATIVE "${PROTO_PREFIX}"
-		"${PROTO_PREFIX}/misc/*.proto"
-		"${PROTO_PREFIX}/video_streaming/*.proto"
-	)
+	file(GLOB PROTOS "${PROTO_PREFIX}/*/*.proto")
 	list(FILTER PROTOS EXCLUDE REGEX "onesie_.*\.proto")
 
-	set(PROTO_FULLPATH ${PROTOS})
-	list(TRANSFORM PROTO_FULLPATH PREPEND "${PROTO_PREFIX}/")
-
-	set(PROTO_C ${PROTOS})
-	list(TRANSFORM PROTO_C REPLACE ".proto" ".pb-c.c")
-	list(TRANSFORM PROTO_C PREPEND "${googlevideo_BINARY_DIR}/")
-
-	add_custom_command(
-		OUTPUT
-			${PROTO_C}
-		COMMAND
-			protoc
-			--proto_path=${PROTO_PREFIX}
-			--c_out=${googlevideo_BINARY_DIR}
-			${PROTO_FULLPATH}
-		DEPENDS
-			${PROTO_FULLPATH}
+	protobuf_generate(
+		TARGET ${target}
+		LANGUAGE c
+		OUT_VAR PROTO_C
+		PROTOC_OUT_DIR ${googlevideo_BINARY_DIR}
+		PROTOS ${PROTOS}
+		IMPORT_DIRS ${PROTO_PREFIX}
+		GENERATE_EXTENSIONS ".pb-c.h" ".pb-c.c"
 	)
 
 	set_source_files_properties(${PROTO_C} PROPERTIES
@@ -43,6 +33,4 @@ macro(generate_googlevideo_protoc target)
 			COMPILE_FLAGS ${DISABLE_CODE_COVERAGE}
 		)
 	endif (BUILD_COVERAGE)
-
-	target_sources(${target} PRIVATE ${PROTO_C})
 endmacro()
