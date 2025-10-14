06b647ec4fcb7e4af9651663121fcbeecd961ad2
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2025-10-14 00:00:33 -0700

    fix clang-tidy job under github CI

--- tests/fuzzer/call_js_foreach.c
+++ tests/fuzzer/call_js_foreach.c
@@ -32,15 +32,16 @@ LLVMFuzzerTestOneInput(const char *data, size_t sz)
 			},
 	};
 
-	char *args[8];
-	args[0] = "fPaFSFklkyAP8IeVM1C";
-	args[1] = "K-qX7Rx6NF8wh-wN_Ni";
-	args[2] = "5KezMq2QMtITgto5cb3";
-	args[3] = "NazoWsDZJa71h_heyXB";
-	args[4] = "Zx9BTcsQimFxwVqtVfF";
-	args[5] = "Kbpbx5yukKR-Px0dhLj";
-	args[6] = "t2yEuJMA6mZh68xBzwE";
-	args[7] = NULL;
+	const char *args[] = {
+		"fPaFSFklkyAP8IeVM1C",
+		"K-qX7Rx6NF8wh-wN_Ni",
+		"5KezMq2QMtITgto5cb3",
+		"NazoWsDZJa71h_heyXB",
+		"Zx9BTcsQimFxwVqtVfF",
+		"Kbpbx5yukKR-Px0dhLj",
+		"t2yEuJMA6mZh68xBzwE",
+		NULL,
+	};
 
 	auto_result r = call_js_foreach(&d, args, &cops, NULL);
 	return 0;
