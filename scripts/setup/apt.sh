0bd2599f682e66d9e1d2f1f1cefa0a91ff4d8fea
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-02-18 16:23:49 -0800

    try using debian unstable for gitlab CI, instead of archlinux, to get access to binary package for quickjs

---
+++ scripts/setup/apt.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+packages=(
+	'clang'
+	'cmake'
+	'gcc'
+	'git'
+	'libada-url-dev'
+	'libc6-dev'
+	'libcurl4-openssl-dev'
+	'libjansson-dev'
+	'libpcre2-dev'
+	'libprotobuf-c-dev'
+	'libseccomp-dev'
+	'libquickjs'
+	'make'
+)
+
+# Install project dependencies via OS package manager
+apt update -qq && apt install -y "${packages[*]}"
+
+# Workaround lack of quickjs pkgconfig metadata
+QIN=./vendor/quickjs.pc.in
+QPC=/usr/lib/pkgconfig/quickjs.pc
+QPREFIX=/usr
+QVERSION="$(apt show libquickjs | grep ^Version | cut -d' ' -f2)"
+m4 -D QUICKJS_PREFIX="$QPREFIX" -D QUICKJS_VERSION="$QVERSION" "$QIN" > "$QPC"
