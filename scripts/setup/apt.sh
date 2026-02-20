8c8b12b4506f0196c64b9f7c5438ec52d290fefb
Eric Woo <9374292-ewoo@users.noreply.gitlab.com>
2026-02-19 23:57:01 -0800

    revert fb38230, dpkg force-unsafe-io did not seem to make a difference

--- scripts/setup/apt.sh
+++ scripts/setup/apt.sh
@@ -21,9 +21,6 @@ packages=(
 	'protobuf-compiler'
 )
 
-# Accelerate batched package install by disabling dpkg fsync()
-echo force-unsafe-io >> /etc/dpkg/dpkg.cfg
-
 # Install project dependencies via OS package manager
 apt-get update -qq
 apt-get install -qqy "${packages[@]}" > /dev/null
